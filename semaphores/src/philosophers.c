9a51d469ac55bd9347ce03b5bc9ce1577867e066
Maciej Walaszczyk <walaszczykm@gmail.com>
2016-11-22 09:14:41 +0100

    signals usage SIGUSR1 SIGABRT

--- semaphores/src/philosophers.c
+++ semaphores/src/philosophers.c
@@ -3,11 +3,13 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <signal.h>
 
 #define PHILOSOPHERS_NUM 5
 #define PHILOSOPHERS_EATING_TIME 5
 #define PHILOSOPHERS_THINKIG_TIME 2
 
+pthread_t threads[PHILOSOPHERS_NUM];
 sem_t forks_sem;
 sem_t waiter_sem;
 char* philosophers_status[PHILOSOPHERS_NUM];
@@ -44,19 +46,60 @@ void philosopher_func(void* arg)
 
 void update_table()
 {
-	system("clear");
 	for(int i=0; i<PHILOSOPHERS_NUM; i++)
 	{
 		printf("[%d]: %s\n", i, philosophers_status[i]);
 	}
 }
 
-int main()
+void clean()
 {
-	pthread_t threads[PHILOSOPHERS_NUM];
+	for(int i=0; i<PHILOSOPHERS_NUM; i++)
+	{
+		if(pthread_cancel(threads[i]) != 0)
+		{
+			perror("pthread_cancel");
+			exit(1);
+		}
+	}
 
+	sem_destroy(&forks_sem);
+	sem_destroy(&waiter_sem);
+
+	exit(0);
+}
+
+void sig_handler(int sig)
+{
+	if(sig == SIGABRT)
+	{
+		printf("[SIGABRT]\n");
+		clean();
+		
+	}
+	else if(sig == SIGUSR1)
+	{
+		printf("[SIGUSR1]\n");
+		update_table();
+	}
+}
+
+void assign_sig_handler(int sig)
+{
+	if(signal(sig, sig_handler) == SIG_ERR)
+	{
+		perror("signal");
+		exit(1);
+	}
+}
+
+void main()
+{
 	sem_init(&forks_sem, 0, PHILOSOPHERS_NUM);
 	sem_init(&waiter_sem, 0, PHILOSOPHERS_NUM-1);
+
+	assign_sig_handler(SIGABRT);
+	assign_sig_handler(SIGUSR1);
 
 	for(int i=0; i<PHILOSOPHERS_NUM; i++)
 	{
@@ -66,15 +109,12 @@ int main()
 		(void *)i) != 0)
 		{
 			perror("thread_create");
-			return 1;
+			exit(1);
 		}
 	}
 
 	while(1)
 	{
 		sleep(1);
-		update_table();
 	}
-
-	return 0;
 }
